1d8ae6e8c44da7eae3098b6e618b3f959d710262
Jeff Palm <jeff@jeffpalm.com>
2024-01-12 07:36:11 -0500

    deploy

--- loomp3r/index.js
+++ loomp3r/index.js
@@ -1,9 +1,5 @@
-/*
 import WaveSurfer from "https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js";
 import RegionsPlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/regions.esm.js";
-*/
-import WaveSurfer from "wavesurfer.js";
-import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
 
 // XXX: Hack to allow interaction and listen to region-out
 let disableRegionOut = false;
